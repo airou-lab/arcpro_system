7e9356135de8f73daa94bc351d1d9965cffb63e7
Arika <arikak@ou.edu>
2025-12-04 01:10:43 +0000

    updated the rl run file

---
+++ arcpro_rl.sh
@@ -0,0 +1,12 @@
+# 1. Start inference server
+cd src/arc_rl_interface/
+python inference_server_RNN.py --model final_model_fresh.zip --img_size 128 128 &
+
+# 2. Move back to workspace root
+cd ../..
+
+# 3. Start robot bridge
+ros2 run bridge real_robot_bridge &
+
+# 4. Start RealSense camera
+ros2 launch realsense2_camera rs_launch.py &
--- rl.sh
+++
@@ -1,8 +0,0 @@
-cd src/arc_rl_interface/
-python inference_server_RNN.py --model final_model_fresh.zip --img_size 128 128
-
-cd ..
-cd ..
-
-
-ros2 run bridge real_robot_bridge
