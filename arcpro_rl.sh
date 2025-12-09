bfc27961de63e9f9cccc5bebec5a19d00c278f16
Arika <arikak@ou.edu>
2025-12-09 22:15:02 +0000

    update folder path names

--- arcpro_rl.sh
+++ arcpro_rl.sh
@@ -1,5 +1,5 @@
 # 1. Start inference server
-cd src/arc_rl_interface/
+cd src/examples/arc_rl_interface
 python inference_server_RNN.py --model final_model_fresh.zip --img_size 128 128 &
 
 # 2. Move back to workspace root
@@ -10,5 +10,5 @@ ros2 run bridge real_robot_bridge &
 
 # 4. Start RealSense camera
 ros2 launch realsense2_camera rs_launch.py &
-
+# Launch vesc
 ros2 launch f1tenth_stack no_lidar_bringup_launch.py sim:=false &
