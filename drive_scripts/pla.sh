160420f32f5f3a429c39c98dc7884f1252209d8a
Arika <arikak@ou.edu>
2026-01-22 01:33:17 +0000

    updating pathing update comments

--- drive_scripts/pla.sh
+++
@@ -1,3 +0,0 @@
-./vesc.sh &
-
-./mockodom.sh
--- drive_scripts/teleop.sh
+++ drive_scripts/teleop.sh
@@ -1,7 +1,7 @@
 # ros2 topic pub /commands/servo/position std_msgs/msg/Float64 '{data: 0.0}' --once #if iw centre the wheels b4 hand
-cd ~/Vnavros2setup/workspaces/f1tenth_ws/src
-source install/setup.sh
-ros2 launch launches teleop.launch.py joy_dev:=ttyUSB0
-
-
-#TODO merge into main repo later!
+#cd ~/Vnavros2setup/workspaces/f1tenth_ws/src
+#source install/setup.sh
+#ros2 launch launches teleop.launch.py joy_dev:=ttyUSB0
+#
+#
+##TODO merge into main repo later!
