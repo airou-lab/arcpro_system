541304fbcd50b5d25e43aa6fe162d31b82d8b806
arika <arika.khor@gmail.com>
2026-02-02 16:42:51 -0600

    delete old urdf mods

--- src/base/robot urdf models/convert_xacro_to_urdf.sh
+++
@@ -1,1 +0,0 @@
-xacro "/home/arika/Documents/arcpro/arcpro_system/src/base/robot urdf models/f1tent.xacro" > f1tent.urdf
--- src/base/robot urdf models/rsp_xacro_test.sh
+++
@@ -1,1 +0,0 @@
-ros2 run robot_state_publisher robot_state_publisher --ros-args -p robot_description:="$(cat '/home/arika/Documents/arcpro/arcpro_system/src/base/robot urdf models/f1tent.urdf')"
